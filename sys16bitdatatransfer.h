87ff8cd7b2ce04add1d04928367e846c8b88fd99
alotofstuff <159913351+alotofstuff@users.noreply.github.com>
2024-02-16 17:34:32 -0500

    tiny update

--- sys16bitdatatransfer.h
+++ sys16bitdatatransfer.h
@@ -1,22 +1,28 @@
 #define clockpin 12
 #define datapin 11
-char binval1;
-char binval2;
-char binval3;
-char binval4;
-char binval5;
-char binval6;
-char binval7;
-char binval8;
-char binval9;
-char binval10;
-char binval11;
-char binval12;
-char binval13;
-char binval14;
-char binval15;
-char binval16;
-void datatransfer() {
+String almostbinval;
+uint16_t finalbinval;
+char binval_1;
+char binval_2;
+char binval_3;
+char binval_4;
+char binval_5;
+char binval_6;
+char binval_7;
+char binval_8;
+char binval_9;
+char binval_10;
+char binval_11;
+char binval_12;
+char binval_13;
+char binval_14;
+char binval_15;
+char binval_16;
+pinMode(datapin, OUTPUT);
+pinMode(clockpin, OUTPUT);
+pinMode(clockpin, INPUT);
+pinMode(datapin, INPUT);
+uint16_t datareceive() {
   
   
 }
